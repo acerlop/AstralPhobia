ceea04cb10803c8f5a8c8ff4d3db772db0d78fa2
PabloVideojuegosVR <pjimagu0105@g.educaand.es>
2023-02-07 10:15:30 +0100

    Pasta

---
+++ Assets/Scripts/Menu/Macarrones.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Macarrones : MonoBehaviour
+{
+    public bool conTomate;
+    public bool oreganoDeOrigenCuestionable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ////////////////////////////////
+        /// COMPRAR MAR... ORÉGANO
+        /////////////////////////////// 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
