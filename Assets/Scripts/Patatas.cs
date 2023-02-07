9e194e313b84cec61e38fc32a61884cb9bfec816
PabloVideojuegosVR <pjimagu0105@g.educaand.es>
2023-02-07 09:58:35 +0100

    Cacahuete

---
+++ Assets/Scripts/Patatas.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patatas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ///////////////////////////
+        /// AQUÍ HACEMOS MILANESAS
+        //////////////////////////
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ///////////////////////////////////////////////////
+        /// AQUÍ NOS COMEMOS LAS MILANESAS MIENTRAS QUEDEN
+        ///////////////////////////////////////////////////
+    }
+}
