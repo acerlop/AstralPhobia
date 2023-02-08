5003f5f0b280c52d2df3a869512dbaec454e63a1
Antonio P. Valls <42718738+antoniopvalls@users.noreply.github.com>
2023-02-08 16:23:55 +0100

    Hola que pasa amegos Script
    Añadimos el Script super importante Hola que pasa amegos  para la funcionalidad Seludos.

---
+++ Assets/Scripts/HolaQuePasaAmegos.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaQuePasaAmegos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("guthab");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hola Que pasa amegos soy Antonio P Valls");
+    }
+}
