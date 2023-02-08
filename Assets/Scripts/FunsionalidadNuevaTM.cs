de0f3270908bd5e7fd13d6ddb00cbe3d790e3c12
Antonio P. Valls <42718738+antoniopvalls@users.noreply.github.com>
2023-02-08 16:26:51 +0100

    FunsionalidadNuevaTM
    Añadimos la nueva funcionalidad llamada FunsionalidadNuevaTM que permite a la aplicación funsionar.

---
+++ Assets/Scripts/FunsionalidadNuevaTM.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunsionalidadNuevaTM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Nueva Super funcionalidad super testeada en todo menos en animales");
+    }
+}
